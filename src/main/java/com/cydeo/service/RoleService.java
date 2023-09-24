5fdab2ced20c48eab121b02dd3fb81d5a3665948
Oleh_K <oleh.usa2017@gmail.com>
2023-09-24 12:53:52 -0400

    CRUD operation interfaces

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,21 +1,10 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.RoleDTO;
-import com.cydeo.dto.UserDTO;
-
-import java.util.List;
 
 
-public interface RoleService {
-    //    save, find, delete, findAll
-    RoleDTO save(RoleDTO roleDTO);
 
-    RoleDTO findBy(Long id);
+public interface RoleService extends CrudService<RoleDTO, Long> {
 
-    List<RoleDTO> findAll();
-
-    void delete(RoleDTO role);
-
-    void delete(Long id);
 
 }
