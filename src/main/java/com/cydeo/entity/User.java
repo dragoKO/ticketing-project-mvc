c4e48c4337de06a0a97b6ac97a37b174a7b740b1
Oleh_K <oleh.usa2017@gmail.com>
2023-09-24 10:43:42 -0400

    entity package

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,13 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+
+public class User extends BaseEntity{
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private Role role;
+    private Gender gender;
+}
