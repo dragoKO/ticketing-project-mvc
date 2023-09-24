b441092510dd88fa75d336a3242aea8664f1df8e
Oleh_K <oleh.usa2017@gmail.com>
2023-09-24 10:15:52 -0400

    enum

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -2,13 +2,14 @@ package com.cydeo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/create")
+    @GetMapping("/create")
     public String createUser(){
 
         return "user/create";
