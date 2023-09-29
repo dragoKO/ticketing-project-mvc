c6c5b8628a98233ec62e0c54b8e194b517e7e2e8
Oleh_K <oleh.usa2017@gmail.com>
2023-09-28 20:48:17 -0400

    role drop down

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -24,6 +24,7 @@ public class UserController {
     public String createUser(Model model) {
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
 
         return "user/create";
     }
