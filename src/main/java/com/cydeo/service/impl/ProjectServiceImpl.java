7c29c33287f49a5c0ea3b8e528828680d9bcd19b
Oleh_K <oleh.usa2017@gmail.com>
2023-10-06 19:22:43 -0400

    update delete complete project

---
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,54 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> implements ProjectService {
+
+    @Override
+    public ProjectDTO save(ProjectDTO object) {
+
+        if(object.getProjectStatus() == null){
+            object.setProjectStatus(Status.OPEN);
+        }
+
+        return super.save(object.getProjectCode(),object);
+    }
+
+    @Override
+    public List<ProjectDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void update(ProjectDTO object) {
+
+        ProjectDTO newproject = findById(object.getProjectCode());
+
+        if(object.getProjectStatus() == null)
+            object.setProjectStatus(newproject.getProjectStatus());
+        super.update(object.getProjectCode(),object);
+    }
+
+    @Override
+    public void deleteById(String id) {
+       super.deleteById(id);
+    }
+
+    @Override
+    public ProjectDTO findById(String id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void complete(ProjectDTO project) {
+        project.setProjectStatus(Status.COMPLETE);
+        super.save(project.getProjectCode(),project);
+    }
+}
--- src/main/java/com/google/service/ProjectService.java
+++
@@ -1,7 +0,0 @@
-package com.google.service;
-
-import com.google.dto.ProjectDTO;
-
-public interface ProjectService extends CrudService<ProjectDTO,String> {
-    void completed(ProjectDTO projectDTO);
-}
